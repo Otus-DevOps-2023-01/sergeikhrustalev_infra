dea459e17af5b49000ca95af8c3c057d3ff16a98
Sergei Khrustalev <mr.sergei.khrustalev@gmail.com>
2023-03-22 05:27:26 +0300

    add terraform-2

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -2,7 +2,7 @@
 wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt-get update
+sleep 20
 apt-get install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
-
